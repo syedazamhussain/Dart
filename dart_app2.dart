2823e277ce1bdd1d9a367b0d79dc319704a6ffb4
Syed Azam Hussain <97602092+syedazamhussain@users.noreply.github.com>
2026-05-22 08:59:54 +0530

    Add files via upload

---
+++ dart_app2.dart
@@ -0,0 +1,10 @@
+import "dart:io";
+void main() {
+    stdout.write("Enter value of A:=");
+
+ int a = int.parse(stdin.readLineSync()!);
+ stdout.write("Value is $a");
+     stdout.write("HELLO");
+
+
+}
---
+++ dart_app3.dart
@@ -0,0 +1,17 @@
+class Test{
+  add() {
+    int a =10; int b = 20; 
+    int c = a+b;
+    print("Value of C = $c");
+
+  }
+}
+
+void main() {
+  Test obj = Test();
+  print("1");
+  obj.add();
+    print("2");
+
+
+}
